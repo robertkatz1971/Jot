80dec9668bb8890bb8843bb502df4d42eca9f006
robertkatz1971 <83724236+robertkatz1971@users.noreply.github.com>
2021-08-24 20:02:23 -0400

    Added authentication

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/logout-manual', function () {
+    request()->session()->invalidate();
+});
+
 Route::get('/{any}', [App\Http\Controllers\AppController::class, 'index'])->where('any', '.*');
